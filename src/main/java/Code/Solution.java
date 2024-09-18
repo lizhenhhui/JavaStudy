24953a6287910193be21e4634d53b334bf069e77
xiaohui <1260411812@qq.com>
2024-09-18 09:33:37 +0800

    初始化

--- src/main/java/Code/Solution.java
+++ src/main/java/Code/Solution.java
@@ -1,29 +1,30 @@
 package Code;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 class Solution {
-    public static boolean wordBreak(String s, List<String> wordDict) {
-        int n = s.length();
-        boolean[] dp = new boolean[n+1];
-        dp[0]=true;
-        Set<String> set = new HashSet<>();
-        for (String str : wordDict)
-            set.add(str);
-        for (String str : wordDict) {
-            for (int i = str.length(); i <=n; i++) {
-                dp[i] = dp[i] || (dp[i - str.length()] && set.contains(s.substring(i - str.length(), i)));
+    public int latestTimeCatchTheBus(int[] buses, int[] passengers, int capacity) {
+        Arrays.sort(buses);
+        Arrays.sort(passengers);
+        int pos = 0;
+        int space = 0;
 
+        for (int arrive : buses) {
+            space = capacity;
+            while (space > 0 && pos < passengers.length && passengers[pos] <= arrive) {
+                space--;
+                pos++;
             }
         }
 
-        return dp[n];
-    }
+        pos--;
+        int lastCatchTime = space > 0 ? buses[buses.length - 1] : passengers[pos];
+        while (pos >= 0 && passengers[pos] == lastCatchTime) {
+            pos--;
+            lastCatchTime--;
+        }
 
-    public static void main(String[] args) {
-        wordBreak("applepenapple", Arrays.asList("apple", "pe"));
+        return lastCatchTime;
     }
 }
+
